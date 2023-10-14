e49d5ad18df35e3c5c6201f013e98394510e3af4
Svetlana <tsinovskaya@gmail.com>
2023-10-14 16:09:50 +0400

    Added file 0

---
+++ Task_00/Program.cs
@@ -0,0 +1,7 @@
+﻿/* 00. Напишите программу, 
+которая на вход принимает число и выдаёт его квадрат */
+
+System.Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int result = userNumber * userNumber;
+System.Console.WriteLine(result);
