38a9e100d6cb9be5743da98262b0fa9ef540f0a8
Svetlana <tsinovskaya@gmail.com>
2023-10-15 11:45:50 +0400

    Added file 8

---
+++ Task_8/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу которая на вход принимает число number, 
+а на выходе выводит все чётные числа от 1 до number (включительно),
+разделеные знаком табуляции.
+*/
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    if (i % 2 == 0)
+    System.Console.Write($"{i}     ");
+}
+
+
