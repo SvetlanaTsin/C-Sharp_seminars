8b24ee420f7092443167411096f163ca8333b9e0
Svetlana <tsinovskaya@gmail.com>
2023-10-17 13:58:37 +0400

    Added file 27

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -4,12 +4,30 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16
 */
+
+// ПЕРВОЕ РЕШЕНИЕ (ЧЕРЕЗ ФУНКЦИЮ):
+// Console.Clear();
+// System.Console.WriteLine("Enter the number A: ");
+// int userA = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Enter the number B: ");
+// int userB = Convert.ToInt32(Console.ReadLine());
+
+// double result = Math.Pow(userA, userB);
+
+// System.Console.WriteLine($"{userA}, {userB} -> {result}");
+
+//ВТОРОЕ РЕШЕНИЕ (ЧЕРЕЗ ЦИКЛ):
 Console.Clear();
 System.Console.WriteLine("Enter the number A: ");
 int userA = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Enter the number B: ");
 int userB = Convert.ToInt32(Console.ReadLine());
 
-double result = Math.Pow(userA, userB);
+int result = userA;
+int count = userB;
 
+for (int i = count; i >= 2; i = i - 1)
+{
+    result = result * userA; 
+}
 System.Console.WriteLine($"{userA}, {userB} -> {result}");
