f3042eb95fb63685268e3f3db5e2832fb426fa89
Svetlana <tsinovskaya@gmail.com>
2023-10-23 13:31:24 +0400

    Added file 42

---
+++ Task_41/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+42. Напишите программу, которая преобразует десятичное число в двоичное (%2 и записываем в обратном порядке)
+45 - 101101
+3- 11
+2 - 10
+*/
+
+Console.Clear();
+System.Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+string Binary(int number)
+{
+    string result = ""; // пока нулевой результат в типе string, записывается как пустая строка ""
+    int temp = 0;
+    for (int i = number; i > 0; i /= 2)
+    {
+        temp = i % 2;
+        result = temp + result; // в типе string + дает запись по порядку 2+2=22
+    }
+
+return result;
+}
+
+string userResult = Binary(userNumber);
+System.Console.WriteLine(userResult);
