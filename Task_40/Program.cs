d4c24f308b79ef13b0ff040b3c3d79a7176770db
Svetlana <tsinovskaya@gmail.com>
2023-10-23 12:44:37 +0400

    Added file 40

---
+++ Task_40/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+40. Напишите программу,которая принимает три числа и определяет, может ли существовать треугольник 
+с сторонами такой длины (т.е. сумма двух сторон д.б. больше третье стороны)
+*/
+
+Console.Clear();
+
+System.Console.WriteLine("Введите число A: ");
+int userA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число B: ");
+int userB = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число B: ");
+int userC = Convert.ToInt32(Console.ReadLine());
+
+bool Triangle(int a, int b, int c)
+{
+    bool result = false; //заводим переменную result и по умолчанию делаем = false, будем проверять на истинность
+    if (a + b > c && a + c > b && b + c > a) result = true;
+    return result;
+}
+
+System.Console.WriteLine(Triangle(userA, userB, userC));
+
+
+
+
